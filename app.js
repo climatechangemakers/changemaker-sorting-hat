1744af219ea625d16e7389402a64f310deb2ed22
Chris <chris.alafaa@gmail.com>
2022-03-22 01:29:41 +0100

    Fix echo bot error

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { App } = require("@slack/bolt");
+const { App, LogLevel } = require("@slack/bolt");
 require("dotenv").config()
 // initializes app with bot token and signing secret
 
@@ -7,17 +7,23 @@ const app = new App({
     signingSecret: process.env.SLACK_SIGNING_SECRET,
     socketMode: true,
     appToken: process.env.APP_TOKEN,
+    logLevel: LogLevel.DEBUG,
+// this app is by default listening for events/commands on the /slack/events & /slack/commands path
     endpoints: {
         events: '/slack/events',
         commands: '/slack/commands'
     }
 });
 
+// this app echoes any text sent with the /echo command
 app.command('/echo', async ({ command, ack, say }) => {
     try {
+        // Acknowledge command request
         await ack();
-        say(`${command.text}`)
+
+        await say(`${command.text}`)
     } catch (error) {
+        // Check the details of the error
         console.log('err')
         console.error(error)
     }
